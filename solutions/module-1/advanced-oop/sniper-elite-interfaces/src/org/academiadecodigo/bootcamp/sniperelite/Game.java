c627ec8f959439a43af66d96a4e08b4d7a00f0fc
Christina Hinchin <christina.hinchin@academiadecodigo.org>
2021-06-01 14:52:16 +0100

    added sniper-elite with interfaces solution

---
+++ solutions/module-1/advanced-oop/sniper-elite-interfaces/src/org/academiadecodigo/bootcamp/sniperelite/Game.java
@@ -0,0 +1,77 @@
+package org.academiadecodigo.bootcamp.sniperelite;
+
+import org.academiadecodigo.bootcamp.sniperelite.gameobject.Destroyable;
+import org.academiadecodigo.bootcamp.sniperelite.gameobject.GameObjectFactory;
+import org.academiadecodigo.bootcamp.sniperelite.gameobject.GameObject;
+import org.academiadecodigo.bootcamp.sniperelite.gameobject.decor.Tree;
+import org.academiadecodigo.bootcamp.sniperelite.gameobject.enemy.Enemy;
+import org.academiadecodigo.bootcamp.sniperelite.gameobject.weapons.SniperRifle;
+
+/**
+ * Sniper Elite
+ */
+public class Game {
+
+    public static final double ENEMIE_PROBABILITY = 0.7;
+    public static final int BULLET_DAMAGE = 1;
+
+    private GameObject[] gameObjects;
+    private SniperRifle sniperRifle;
+    private int shotsFired;
+
+    /**
+     * Construct the game
+     * @param numberObjects the number of game objects to create
+     */
+    public Game(int numberObjects) {
+
+        this.gameObjects = createGameObjects(numberObjects);
+        this.sniperRifle = new SniperRifle(BULLET_DAMAGE);
+
+    }
+
+    /**
+     * Start shooting
+     */
+    public void start() {
+
+        for (GameObject gameObject : gameObjects) {
+
+            System.out.println(gameObject.getMessage());
+
+            if (gameObject instanceof Destroyable) {
+
+                Destroyable target = (Destroyable) gameObject;
+                while (!target.isDestroyed()) {
+
+                    System.out.println("Making the shot...");
+                    shotsFired++;
+                    sniperRifle.shoot(target);
+
+                }
+
+                System.out.println("Target is neutralized. I repeat, Target is neutralized!");
+
+            }
+
+        }
+
+        System.out.println("All targets are down. I repeat all targets are down. " + shotsFired + " shots.");
+
+    }
+
+    private GameObject[] createGameObjects(int numberObjects) {
+
+        GameObject[] gameObjects = new GameObject[numberObjects];
+
+        for (int i = 0; i <gameObjects.length ; i++) {
+
+            gameObjects[i] = Math.random() < ENEMIE_PROBABILITY ? GameObjectFactory.createEnemy() : GameObjectFactory.createObject();
+
+        }
+
+        return gameObjects;
+
+    }
+
+}
